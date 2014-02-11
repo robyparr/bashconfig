d21a3f65215432c37c2a6ceb128db5273977600a
Roby Parr <roby.102@hotmail.com>
2014-02-11 19:30:56 +0000

    Added basic deployment and backup batch file.

---
+++ deploy.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+
+REM Configuration Variables
+SET bin_dir=C:\cygwin64\usr\local\
+SET home_dir=C:\cygwin64\home\Roby\
+
+ECHO bin directory: %bin_dir%
+ECHO home directory: %home_dir%
+
+
+IF "%1" == "" GOTO deploy
+IF "%1" == "deploy" GOTO deploy
+IF "%1" == "backup" GOTO backup
+
+REM Catch incorrect params
+GOTO end
+
+:deploy
+ECHO Deploying...
+COPY /-Y bin %bin_dir%
+COPY /-Y .bashrc %home_dir%
+GOTO end
+
+:backup
+ECHO Backing up...
+COPY /-Y %bin_dir%bin bin
+COPY /-Y %home_dir%.bashrc .bashrc
+
+:end
+ECHO Finished.
