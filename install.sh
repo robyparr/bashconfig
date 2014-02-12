abdab195b6fef4533086e2c16a8b7da865087c34
Roby Parr <roby.102@hotmail.com>
2014-02-12 16:39:43 +0000

    Added uninstall capability to install script.

--- install.sh
+++ install.sh
@@ -9,23 +9,34 @@ symlink_bashrc_dest="/home/$USER/.bashrc"
 symlink_bin="/c/Users/$USER/Documents/Projects/bashconfig/bin"
 symlink_bin_dest="/usr/local/bin"
 
-if ! [ -h $symlink_c_dest ]; then
-	ln -s $symlink_c $symlink_c_dest
-	echo "Symlink created from $symlink_c to $symlink_c_dest."
-else
-	echo "Symlink $symlink_c already exists."
-fi
+if [ "$1" == "" -o "$1" == "install" -o "$1" == "-i" ]; then
+	if ! [ -h $symlink_c_dest ]; then
+		ln -s $symlink_c $symlink_c_dest
+		echo "Symlink created from $symlink_c to $symlink_c_dest."
+	else
+		echo "Symlink $symlink_c already exists."
+	fi
 
-if ! [ -h $symlink_bashrc_dest ]; then
-	ln -s $symlink_bashrc $symlink_bashrc_dest
-	echo "Symlink created from $symlink_bashrc to $symlink_bashrc_dest."
-else
-	echo "Symlink $symlink_bashrc already exists."
-fi
+	if ! [ -h $symlink_bashrc_dest ]; then
+		ln -s $symlink_bashrc $symlink_bashrc_dest
+		echo "Symlink created from $symlink_bashrc to $symlink_bashrc_dest."
+	else
+		echo "Symlink $symlink_bashrc already exists."
+	fi
 
-if ! [ -h $symlink_bin_dest ]; then
-	ln -s $symlink_bin $symlink_bin_dest
-	echo "Symlink created from $symlink_bin to $symlink_bin_dest."
+	if ! [ -h $symlink_bin_dest ]; then
+		ln -s $symlink_bin $symlink_bin_dest
+		echo "Symlink created from $symlink_bin to $symlink_bin_dest."
+	else
+		echo "Symlink $symlink_bin already exists."
+	fi
+
+elif [ "$1" == "uninstall" -o "$1" == "-u" ]; then
+	unlink $symlink_bin_dest
+	unlink $symlink_bashrc_dest
+	unlink $symlink_c_dest
+	echo "Symbolic links removed."
+
 else
-	echo "Symlink $symlink_bin already exists."
+	echo "Unknown argument."
 fi
