5f294354061e4affd616db2b57564b42c4836ac1
Roby Parr <roby.102@hotmail.com>
2014-02-12 16:51:09 +0000

    Removed file extension from scripts.

---
+++ bin/sublime
@@ -0,0 +1,3 @@
+# Run the sublime text editor program.
+# $1 is the name of a file to open.
+"/c/Program Files/Sublime Text 2/sublime_text.exe" $1 &
--- bin/sublime.sh
+++
@@ -1,3 +0,0 @@
-# Run the sublime text editor program.
-# $1 is the name of a file to open.
-"/c/Program Files/Sublime Text 2/sublime_text.exe" $1 &
